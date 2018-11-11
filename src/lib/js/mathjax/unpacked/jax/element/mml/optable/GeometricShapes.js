d93c003c747594146311244f9d78d28c185582dd
AksharaMayreddy <44540262+AksharaMayreddy@users.noreply.github.com>
2018-11-11 11:48:37 +0530

    Update GeometricShapes.js

--- src/lib/js/mathjax/unpacked/jax/element/mml/optable/GeometricShapes.js
+++ src/lib/js/mathjax/unpacked/jax/element/mml/optable/GeometricShapes.js
@@ -28,14 +28,14 @@
         '\u25A0': MO.BIN3,     // black square
         '\u25A1': MO.BIN3,     // white square
         '\u25AA': MO.BIN3,     // black small square
-        '\u25AB': MO.BIN3,     // white small square
+        "\u25AB": MO.BIN3,     // white small square
         '\u25AD': MO.BIN3,     // white rectangle
         '\u25AE': MO.BIN3,     // black vertical rectangle
         '\u25AF': MO.BIN3,     // white vertical rectangle
         '\u25B0': MO.BIN3,     // black parallelogram
         '\u25B1': MO.BIN3,     // white parallelogram
         '\u25B2': MO.BIN4,     // black up-pointing triangle
-        '\u25B4': MO.BIN4,     // black up-pointing small triangle
+        "\u25B4": MO.BIN4,     // black up-pointing small triangle
         '\u25B6': MO.BIN4,     // black right-pointing triangle
         '\u25B7': MO.BIN4,     // white right-pointing triangle
         '\u25B8': MO.BIN4,     // black right-pointing small triangle
@@ -51,7 +51,7 @@
         '\u25C8': MO.BIN4,     // white diamond containing black small diamond
         '\u25C9': MO.BIN4,     // fisheye
         '\u25CC': MO.BIN4,     // dotted circle
-        '\u25CD': MO.BIN4,     // circle with vertical fill
+        "\u25CD": MO.BIN4,     // circle with vertical fill
         '\u25CE': MO.BIN4,     // bullseye
         '\u25CF': MO.BIN4,     // black circle
         '\u25D6': MO.BIN4,     // left half black circle
